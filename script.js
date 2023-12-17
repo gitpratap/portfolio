c22d8cedafbeea309a11fe77bb6508e82cc62af1
gitpratap <mbhanupratap720@gmail.com>
2023-12-16 20:30:28 -0500

    second commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new typed(".text",{
+    String$:["Frontend Devloper", "Desogner", "Web devloper"],
+    typespeed: 100,
+    backspeed: 100,
+    backDelay: 1000,  
+    loop: true
+
+})
